b59d090b391afe3c277550b687e49e4bcb439752
helgeanl <helgelang@gmail.com>
2016-09-14 23:25:32 +0200

    Update driv_UART.c

--- src/Node1/driv_UART.c
+++ src/Node1/driv_UART.c
@@ -18,19 +18,19 @@ unsigned char usart_receive(void)
 void usart_transmit( unsigned char data )
 {
 	/* Wait for empty transmit buffer */
-    while( !( UCSR0A & (1<<UDRE0)) );
-    /* Put data into buffer, sends the data */
-    UDR0 = data;
+	while( !( UCSR0A & (1<<UDRE0)) );
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
 }
 
 
 void usart_init(uint8_t ubrr)
 {
 	/* Set baud rate*/
-    UBRR0L = ubrr;
+	UBRR0L = ubrr;
 	/* Enable receiver and transmitter*/
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-    /* Enable printf */
+	/* Enable printf */
 	fdevopen(usart_transmit, usart_receive);
 }
 
