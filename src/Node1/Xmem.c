f1e605d3d04ff8f39435b39a0cb06fd7ad82b17e
helgeanl <helgelang@gmail.com>
2016-09-14 22:57:52 +0200

    Update Xmem.c

--- src/Node1/Xmem.c
+++ src/Node1/Xmem.c
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 
 void xmem_init(){
-	MCUCR |= (1<<SRE);
-	SFIOR |= (1<<XMM2);
-	MCUCR |= (1<<SRW10);
-	EMCUCR |= (1<<SRW11);
+	MCUCR |= (1<<SRE); 	// Enable external memory
+	SFIOR |= (1<<XMM2); 	// Mask out addr. A12-A15 (are used as JTAG pins instead)
+	MCUCR |= (1<<SRW10); 	// Add wait cycle on read/write
+	EMCUCR |= (1<<SRW11); 	// Add wait cycle on read/write
 }
