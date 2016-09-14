f02590a30e315035e2e54913b5e37464ad5f1645
helgeanl <helgelang@gmail.com>
2016-09-14 23:06:19 +0200

    Update main.c

--- src/Node1/main.c
+++ src/Node1/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  * Created: 27.08.2015 12:50:37
- * Author: Helge-André Langåker, Johan Vårlid
+ * Author: Helge-Andr≈Ω Lang√•ker, Johan V√•rlid
  */ 
 
 #include <util/delay.h>
@@ -27,45 +27,44 @@ int main(void)
 	DDRC = (1<< DDC0)|(1<< DDC1)|(1<< DDC2)|(1<< DDC3);
 	DDRE = (1<< DDE1);
 	DDRD = (1<< DDD6)|(1<<DDD7);
-    
-    // Set relay signal pin to output
+	
+	// Set relay signal pin to output
     DDRD |= (1<<PD4);
     clear_bit(DDRD,PD4);
 	
-    // Initialization of drivers
+    	// Initialization of drivers
 	usart_init(MYUBRR);
 	xmem_init();
 	joy_init();
 	oled_init();
     can_init();
     fsm_init();
-    sei();
-	printf("Test\n");
+    sei(); 		// Enable interrupts
 	
 	// Button and joystick states
-	joy_direction_t current_state_joy	= NEUTRAL;
-	joy_direction_t previous_state_joy  = NEUTRAL;
-	uint8_t current_state_left_but		= 0;
-	uint8_t previous_state_left_but     = 0;
-	uint8_t current_state_right_but     = 0;
-	uint8_t previous_state_right_but	= 0;
-	uint8_t previous_state_joy_but		= 0;
-	uint8_t current_state_joy_but		= 0;
+	joy_direction_t current_state_joy		= NEUTRAL;
+	joy_direction_t previous_state_joy  	= NEUTRAL;
+	uint8_t current_state_left_but			= 0;
+	uint8_t previous_state_left_but     	= 0;
+	uint8_t current_state_right_but     	= 0;
+	uint8_t previous_state_right_but		= 0;
+	uint8_t previous_state_joy_but			= 0;
+	uint8_t current_state_joy_but			= 0;
 	
 	// Joystick
 	joy_position_t      joy_pos;
     
-    // CAN
+	 // CAN
     can_message_t       joy_pos_msg;
     can_message_t       slider_pos_msg;
     can_message_t       received_msg;
     
-	
-    while(1)
-    {
+	while(1)
+	{
         // Receive message from CAN
-        if(mcp2515_read(MCP_CANSTAT) & 0x0C || mcp2515_read(MCP_CANSTAT) & 0x0E){ 
-            can_message_receive(&received_msg);
+        if(mcp2515_read(MCP_CANSTAT) & 0x0C || mcp2515_read(MCP_CANSTAT) & 0x0E)
+        { 
+        	can_message_receive(&received_msg);
             fsm_can(&received_msg);
         }
 		        
@@ -74,16 +73,18 @@ int main(void)
 		if (current_state_left_but != previous_state_left_but)
 		{	
 			previous_state_left_but = current_state_left_but;
-			if(joy_button(LEFT_BUTTON)){
-                fsm_left_button();
+			if(joy_button(LEFT_BUTTON))
+			{
+            	fsm_left_button();
 			}
 		}
 		current_state_right_but = joy_button(RIGHT_BUTTON); // Right button
 		if (current_state_right_but != previous_state_right_but)
 		{	
 			previous_state_right_but = current_state_right_but;
-			if(joy_button(RIGHT_BUTTON)){
-                fsm_right_button();
+			if(joy_button(RIGHT_BUTTON))
+			{
+        		fsm_right_button();
 			}
 		}
 		
@@ -91,31 +92,36 @@ int main(void)
 		if (current_state_joy_but != previous_state_joy_but)
 		{
 			previous_state_joy_but = current_state_joy_but;
-			if(joy_button(JOY_BUTTON)){
-                fsm_joy_button();
+			if(joy_button(JOY_BUTTON))
+			{
+            	fsm_joy_button();
 			}
 		}
 		
 		//  Event handling of joystick
 		current_state_joy = joy_get_direction();
-		if(current_state_joy !=previous_state_joy){
+		if(current_state_joy !=previous_state_joy)
+		{
 			previous_state_joy=current_state_joy;
-            if( joy_get_direction() == DOWN){
-                fsm_joy_down();
+            if( joy_get_direction() == DOWN)
+            {
+            	fsm_joy_down();
 			}
-			else if(joy_get_direction() == UP){
-                fsm_joy_up();
+			else if(joy_get_direction() == UP)
+			{
+            	fsm_joy_up();
 			}
-            if(joy_get_direction() == RIGHT){
-                fsm_joy_right();
+			if(joy_get_direction() == RIGHT)
+            {
+        		fsm_joy_right();
 			}
-            if(joy_get_direction() == LEFT){
-                fsm_joy_left();
+            if(joy_get_direction() == LEFT)
+            {
+            	fsm_joy_left();
 			}
 		}
         
         fsm_refresh_screen();
-    
 		_delay_ms(10);
-    }
+	}
 }
