38ba9f52a7f67c2281a4c2dcfcfdb02522dacebd
helgeanl <helgelang@gmail.com>
2016-09-14 23:56:44 +0200

    Update setup.h

--- src/Node1/setup.h
+++ src/Node1/setup.h
@@ -13,10 +13,9 @@
 #define F_OSC   4915200
 #define F_CPU   F_OSC
 #define BAUD    9600
-//#define MYUBRR (F_OSC-250)/16/BAUD-1
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR  FOSC/16/BAUD-1
 
-//Added from the project description to help setting bits in registers
+// Added from the project description to help setting bits in registers
 #define set_bit(reg,bit) (reg |= (1 << bit))
 #define clear_bit(reg,bit) (reg &= ~(1 << bit))
 #define test_bit( reg, bit ) (reg & (1 << bit))
